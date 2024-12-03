d7f0f3cd9d2174b703110ddef8d274efa8255223
vinuwilson <vinu5732@gmail.com>
2024-12-03 11:51:02 +0000

    Updated the Result DTO to reflect changes in the values returned by the API

--- app/src/main/java/com/vinuw/moviecollection/data/dto/ResultDto.kt
+++ app/src/main/java/com/vinuw/moviecollection/data/dto/ResultDto.kt
@@ -4,28 +4,28 @@ import com.vinuw.moviecollection.domain.model.ResultModel
 
 data class ResultDto(
     val adult: Boolean,
-    val backdrop_path: String,
+    val backdrop_path: String?,
     val genre_ids: List<Int>,
     val id: Int,
-    val original_language: String,
-    val original_title: String,
-    val overview: String,
+    val original_language: String?,
+    val original_title: String?,
+    val overview: String?,
     val popularity: Double,
-    val poster_path: String,
-    val release_date: String,
-    val title: String,
+    val poster_path: String?,
+    val release_date: String?,
+    val title: String?,
     val video: Boolean,
     val vote_average: Double,
     val vote_count: Int
 )
 
 fun ResultDto.toResultModel() = ResultModel(
-    backdropPath = backdrop_path,
+    backdropPath = backdrop_path ?: "",
     id = id,
-    originalLanguage = original_language,
-    originalTitle = original_title,
-    overview = overview,
-    posterPath = poster_path,
-    releaseDate = release_date,
-    title = title
+    originalLanguage = original_language ?: "",
+    originalTitle = original_title ?: "",
+    overview = overview ?: "",
+    posterPath = poster_path ?: "",
+    releaseDate = release_date ?: "",
+    title = title ?: ""
 )
